e9e9422e3216e189700fd82af2d815becb5823d2
Piyush <piyushlekhak@gmail.com>
2024-03-04 00:09:36 +0545

    changed repo and edited admin.

--- server/server/settings.py
+++ server/server/settings.py
@@ -200,7 +200,7 @@ JAZZMIN_UI_TWEAKS = {
     "sidebar_nav_compact_style": False,
     "sidebar_nav_legacy_style": False,
     "sidebar_nav_flat_style": False,
-    "theme": "cyborg",
+    "theme": "slate",
     "dark_mode_theme": None,
     "button_classes": {
         "primary": "btn-primary",
